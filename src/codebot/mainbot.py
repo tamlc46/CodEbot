06434ae591511d25cb2c07767cfe98b66a127c9b
Nguyen Vuong <vlmnguyen98@gmail.com>
2020-04-29 23:39:42 +0700

    Add more data and tuning models

--- src/codebot/mainbot.py
+++ src/codebot/mainbot.py
@@ -1,40 +1,26 @@
 # -*- coding: utf-8 -*-
 
-import os
-import re
+from preprocessing import *
+from utilities import *
 import sys
 import traceback
-from utilities import *
-import joblib
 
 # Set terminal encoding to utf-8
 sys.stdin.reconfigure(encoding='utf-8')
 sys.stdout.reconfigure(encoding='utf-8')
 
 class codebot:
-    def __init__(self, kbmodel_dir_path=""):
-        if kbmodel_dir_path == "":
-            kbmodel_dir_path = os.path.dirname(
-                os.path.abspath(__file__)) + "/KB"
+    def __init__(self):
         
-        # Knowledge base:
-        # self.kb = read_json(f'{kbmodel_dir_path}/knowledge.json')  # kb[term][context][intent]
-        # self.rb = read_json(f'{kbmodel_dir_path}/response.json')   # rb[relevance][intent]
-
-        # Preprocessing Model:
-        self.preprocessor = None
-
-        # Linear Support Vector Classifiers:
-        self.intent_clf = None
-        self.relevance_clf = None
-        self.context_clf = None
+        # self.__load_KB()
+        self.__load_models()
 
         # State Information:
-        self.input_queue = []
+        self.raw_queue = []
+        self.vec_queue = []
         self.state = {
+            'intent': None,         # Intent of user
             'context': 'general',   # Context is for looking up the right entity in kb
-            'relevance': False,     # Relevance is whether the input was about programming or not
-            'intent': None          # Intent of user
         }
     
     def __repr__(self):
@@ -43,16 +29,55 @@ class codebot:
             self.__class__.__name__,
             hex(id(self))
         )
-
-    def read(self, input_str):
-        pass
-
-    def answer(self):
-        return "Seen!"
     
+    def __load_KB(self):
+        self.kb = read_json('./KB/knowledge.json')             # kb[term][context][intent]
+        self.rb = read_json('./KB/dir_path}/response.json')    # rb[relevance][intent]
+
+    def __load_models(self):
+        # Preprocessing Model:
+        self.preprocessor = joblib.load('./model/preprocessing.bin')
+
+        # Linear Support Vector Classifiers:
+        self.intent_clf = joblib.load('./model/model_intent.bin')
+        self.context_clf = joblib.load('./model/model_context.bin')
+
+    def answer(self, input_str):
+        input_sentences = list(filter(lambda s: len(s) , re.sub('[\.\?\!]', '\n', input_str).split('\n')))
+        if len(input_sentences) > 1:
+            return "Tôi không biết phải bắt đầu từ đâu cả. Tôi không thể xử lý nhiều câu hỏi cùng lúc :("
+        
+        # Push in queues:
+        self.raw_queue.append(input_str)
+        self.vec_queue.append(self.preprocessor.transform(pd.Series(input_sentences)))
+
+        # Process queues:
+        X = self.vec_queue[0]
+        self.state['context'] = (self.context_clf.predict(X)[0], np.max(self.context_clf.decision_function(X)))
+        self.state['intent'] = (self.intent_clf.predict(X)[0], np.max(self.intent_clf.decision_function(X)))
+
+        # Empty queues:
+        self.raw_queue = []
+        self.vec_queue = []
+
+        # Get answer:
+        answer = ''
+        return self.state
 
 if __name__ == "__main__":
     bot_instance = codebot()
-    while True:
-        bot_instance.read(input())
-        print(bot_instance.answer())
+    running = True
+    while running:
+        try:
+            print(bot_instance.answer(input()))
+        except Exception as ex:
+            running = False
+            print(bot_instance.raw_queue, bot_instance.state)
+            print("An error has occured! Here is the error details:")
+            try:
+                traceback.print_tb(ex.__traceback__, file=sys.stdout)
+                print(ex)
+            except Exception as another_ex:
+                print("Another error has occured::", another_ex)
+        finally:
+            pass
---
+++ src/codebot/preprocessing.py
@@ -0,0 +1,60 @@
+import os
+import errno
+import shutil
+import time
+import random
+import re
+
+import pandas as pd
+import numpy as np
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from underthesea import word_tokenize
+
+import joblib
+
+class Preprocessing:
+    def __init__(self, input='content', encoding='utf-8', decode_error='strict', strip_accents=None, 
+                 lowercase=True, preprocessor=None, tokenizer=None, analyzer='word', stop_words=None, 
+                 token_pattern='(?u)\b\w\w+\b', ngram_range=(1, 1), max_df=1.0, min_df=1, max_features=None, 
+                 vocabulary=None, binary=False, dtype=np.float64, norm='l2', use_idf=True, smooth_idf=True, 
+                 sublinear_tf=False):
+        if (tokenizer == None):
+            self._tokenizer = word_tokenize
+        else:
+            self._tokenizer = tokenizer
+        
+        self._vectorizer = TfidfVectorizer(
+            input=input, encoding=encoding, decode_error=decode_error, strip_accents=strip_accents, 
+            lowercase=lowercase, preprocessor=preprocessor, tokenizer=self._tokenizer, analyzer=analyzer, 
+            stop_words=stop_words, token_pattern=token_pattern, ngram_range=ngram_range, 
+            max_df=max_df, min_df=min_df, max_features=max_features, vocabulary=vocabulary, binary=binary, 
+            dtype=dtype, norm=norm, use_idf=use_idf, smooth_idf=smooth_idf, sublinear_tf=sublinear_tf)
+        
+    def fit(self, df):
+        self._vectorizer.fit(self.optional_preprocess(df))
+        return self
+    
+    def fit_transform(self, df):
+        return self._vectorizer.fit_transform(self.optional_preprocess(df))
+        
+    def transform(self, df):
+        """
+        Vectorize Pandas DataFrame
+        """
+        return self._vectorizer.transform(self.optional_preprocess(df))
+    
+    def optional_preprocess(self, df):
+        return df.apply(lambda q: re.sub('[cC]\+\+', 'cpp', q))
+    
+    def save(self, file_name):
+        print("Saving model to ", file_name)
+        return joblib.dump(self, file_name)
+        
+    @staticmethod
+    def load(file_name):
+        if (isinstance(file_name, str) and os.path.isfile(file_name)):
+            self = joblib.load(file_name)
+        else:
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), file_name)
+        return self
